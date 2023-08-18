853d749eeb25f76a2358a1f1e6344e378d61b1d4
tinghsuanhsu <tinghsuanhsu@gmail.com>
2023-08-18 12:43:48 +1000

    Create a js file to manipulate HTML elements

---
+++ DOM_Practice/js-file.js
@@ -0,0 +1,31 @@
+/*
+Hint for this one: after creating the <div> with createElement, 
+append the <h1> and <p> to it before adding it to the container.
+*/
+
+//a <p> with red text that says “Hey I’m red!”
+const body = document.body
+const redParagraph = document.createElement('p');
+redParagraph.textContent = 'I\'m a blue h3';
+redParagraph.style.cssText = 'color:red';
+body.append(redParagraph);
+
+//an <h3> with blue text that says “I’m a blue h3!”
+const blueText = document.createElement('h3')
+blueText.textContent = 'I\'m a blue h3'
+blueText.style.cssText = 'color:blue';
+body.append(blueText)
+
+//a <div> with a black border and pink background color with the following elements inside of it:
+const div = document.createElement('div')
+body.append(div)
+div.style.backgroundColor = 'pink'
+
+
+// another <h1> that says “I’m in a div”
+// a <p> that says “ME TOO!”
+const h1Text = document.createElement('h1')
+h1Text.textContent = 'I\'m in a div'
+const meToo = document.createElement('p')
+meToo.textContent = 'ME TOO!'
+div.append(h1Text, meToo)
